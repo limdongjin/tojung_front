89d25c25e5c636bdf53c0b4639edcd5fe0b89fa0
limdongjin <geniuslim27@gmail.com>
2019-02-01 05:58:51 +0900

    css import optimization

--- src/main.js
+++ src/main.js
@@ -9,14 +9,10 @@ import "bootstrap";
 import "bootstrap/js/dist/modal";
 import "bootstrap/js/dist/tooltip";
 import "bootstrap/js/dist/carousel";
-import "bootstrap/scss/bootstrap.scss";
 import "expose-loader?$!expose-loader?jQuery!jquery";
-import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick.min.js";
-import "vue-popperjs/dist/vue-popper.css";
 import "vue-popperjs/dist/vue-popper.min.js";
 import "@/assets/regacy.scss";
-// import Regacy from "@/assets/regacy.js";
 
 // global.$ = $;
 Vue.config.productionTip = false;
